33cbb99c343d2999ec26c53a2c294f70d2d5de8f
SunNinjs <coolmanpower1@gmail.com>
2021-03-19 21:57:57 -0700

    fixed vote-clear

--- commands/vote-clear.js
+++ commands/vote-clear.js
@@ -6,8 +6,9 @@ module.exports = {
 	cooldown: 1,
 	description: 'Clears all the votes',
     permissions: 'ADMINISTRATOR',
-	execute(message, args, Votes) {
+	execute(message, args, Votes, Voters) {
         Votes.clear();
+		Voters.clear();
 		message.channel.send(`The leaderboard has been cleared`);
 	},
 };
